4777a2e967d8ea8f2f6356ce3ecedda7b95425a4
Matthew Seman <dev@mseman.com>
2022-05-11 16:37:40 -0500

    Persist subscriptions in the DB

--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,17 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-use crate::routes::{health_check,subscribe};
+use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> std::io::Result<Server> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection_pool: PgPool) -> std::io::Result<Server> {
+    let connection_pool = web::Data::new(connection_pool);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection_pool.clone())
     })
     .listen(listener)?
     .run();
